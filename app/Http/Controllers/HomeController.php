9976142e392320bd928fb73ddc2e6e3af04e5071
Mathius <mathiuskormasela12@gmail.com>
2023-08-03 22:55:03 +0700

    init app

---
+++ app/Http/Controllers/HomeController.php
@@ -0,0 +1,166 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Warga;
+
+class HomeController extends Controller
+{
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    //public function dashboard()
+    //{
+    //    return view('admin.layout.dashboard-layout');
+    //}
+
+    public function index1()
+    {
+        return view('home');
+    }
+
+    public function index()
+    {
+        return view('admin.layout.home-content');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+
+     public function datawarga()
+     {
+        $data=[];
+        $data['warga'] = Warga::all();
+         return view('admin.layout.datawarga-layout',$data);
+     }
+     
+    public function create()
+    {
+        return view('admin.layout.insert-data');
+    }
+
+    //public function simpan(Request $request)
+    //{
+     //   $warga = new Warga();
+     //   $warga->nama = $request->input('nama');
+     //   $warga->email = $request->input('email');
+     //   $warga->alamat = $request->input('alamat');
+     //   $warga->save();
+
+      //  return redirect()->back()->with('success', 'Data warga berhasil ditambahkan');
+    //}
+
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $warga = new Warga();
+        $warga->nama = $request->get('nama');
+        $warga->email = $request->get('email');
+        $warga->alamat = $request->get('alamat');
+        $warga->save();
+
+        if($warga){
+            return redirect()->route('warga.data')->with(['success'=> 'Data warga berhasil ditambahkan']);
+
+        } else{
+            return redirect()->route('warga.data')->with(['error'=> 'Data warga gagal disimpan']);
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Request $request,$id)
+    {
+        //
+        //$warga = Warga::find(Request $request, $id)
+        //dd($warga);
+        //return view('admin.layout.datawarga-layout', compact(['warga']));
+        $warga = Warga::where('id',$request->id)->first();
+        return view('admin.layout.datawarga-layout', compact('warga'));
+    }
+
+
+    public function ubah($id)
+    {
+        //
+        $warga = Warga::find($id);
+        dd($warga);
+        return view('admin.layout.edit', compact(['warga']));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+        $warga = Warga::find($id);
+        $warga->nama = $request->get('nama');
+        $warga->email = $request->get('email');
+        $warga->alamat = $request->get('alamat');
+        $warga->save();
+
+        if($warga){
+            return redirect()->route('warga.data')->with(['success'=> 'Data warga berhasil diubah']);
+
+        } else{
+            return redirect()->route('warga.data')->with(['error'=> 'Data warga gagal disimpan']);
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+        $data=[];
+        $data = Warga::where('id',$id)-> delete();
+
+        if($data){
+            return redirect()->route('warga.data')->with(['success'=> 'Data warga berhasil dihapus']);
+
+        } else{
+            return redirect()->route('warga.data')->with(['error'=> 'Data warga gagal dihapus']);
+        }
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,41 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+//Route::get('/', function () {
+ //  return view('welcome');
+//});
+//Route::get('/',[\App\Http\Controllers\DashController::class,'index']);
+
+Route::middleware(['auth'])->group(function(){
+Route::get('/',[App\Http\Controllers\HomeController::class,'index']);
+Route::get('/1',[App\Http\Controllers\HomeController::class,'dashboard']);
+Route::get('/admin/datawarga',[App\Http\Controllers\HomeController::class,'datawarga'])->name('warga.data');
+Route::get('/admin/insert-data',[App\Http\Controllers\HomeController::class,'create']);
+
+Route::post('/admin/datawarga',[App\Http\Controllers\HomeController::class,'store'])->name('warga.insert');
+Route::post('/admin/update-data/{id}',[App\Http\Controllers\HomeController::class,'update'])->name('warga.update');
+Route::get('/admin/delete-data/{id}',[App\Http\Controllers\HomeController::class,'destroy']);
+
+//Route::get('/admin/edit/{id}/',[App\Http\Controllers\HomeController::class,'ubah']);
+
+Route::get('/admin/edit-data/{id}/',[App\Http\Controllers\HomeController::class,'edit'])->name('edit');
+
+//Route::get('/admin/datawarga/create', [HomeController::class, 'create'])->name('datawarga.create');
+////
+
+});
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index1'])->name('home');
